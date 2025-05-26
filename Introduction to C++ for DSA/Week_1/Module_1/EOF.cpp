bb2c8c98489e05087f509fb8df2fa312fb4bb922
Md. Nazmul Huda Labib <nazmullabib012@gmail.com>
2025-05-26 13:59:59 +0600

    Module_1 is done of week_1
    Basic C++ operations

---
+++ Introduction to C++ for DSA/Week_1/Module_1/EOF.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+int32_t main()
+{
+    fast_io;
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        int x;
+        while (cin >> x)
+        {
+            cout<<x<<endl;
+        }
+    }
+    return 0;
+}
